dec5417a688d56e634d74483d3c3824f58f48d15
NIXONHS <111463005+NIXONHS1@users.noreply.github.com>
2024-06-17 17:32:57 -0500

    Delete App.java

--- App.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto;
-
-public class App {
-    public static void main(String[] args) {
-        // Aplicacion llama al menu principal y recibe de parametro un GestorJuego
-        MenuJuego menu = new MenuJuego(new Juego());
-        menu.menuPrincipal();
-    }
-}
